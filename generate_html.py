190c2e91ec56be6224a7743700a7fe56d5ac8769
Thinsuu <thinyadanarsu@gmail.com>
2024-08-03 00:15:43 +0200

    Show wind data on the webpage

--- generate_html.py
+++ generate_html.py
@@ -13,7 +13,7 @@ from pathlib import Path
 OUTPUT_DIR = Path('publish')
 
 
-def prepare_json():
+def prepare_json_temp():
     diff_of_interest = list(range(2, 100))
 
     max_historical_date = session.query(func.max(HistoricalData.local_date)).scalar()
@@ -37,101 +37,93 @@ def prepare_json():
     for result in db_results:
         date = result.local_date
         if date not in data:
-            data[date] = {'Actual': result.actual_temp}
+            data[date] = {'Actual_temp': result.actual_temp}
             for diff_time in diff_of_interest:
                 data[date][diff_time] = None
-        data[date][result.time_difference] = (data[date]["Actual"] / result.forecast_temp) * 100.0 - 100.0
-
+        data[date][result.time_difference] = (data[date]["Actual_temp"] / result.forecast_temp) * 100.0 - 100.0
+    
     df = pd.DataFrame.from_dict(data, orient='index')
     df = df.rename_axis('actual_date').reset_index()
     df = df.groupby(df['actual_date'].dt.date).mean()
     df = df.drop('actual_date', axis=1).sort_index(ascending=False)
-
-    output_data = {
-        date.strftime('%Y-%m-%d'): {
-            'Actual': f'{values_dict["Actual"]:.1f}',
-            'Trend': list(
-                (None if math.isnan(v) else round(v, 2))
-                for k, v in values_dict.items() if k != 'Actual'
-            ),
+    
+    temp_output_data = {
+            date.strftime('%Y-%m-%d'): {
+                'Temp_Actual': f'{values_dict["Actual_temp"]:.1f}',
+                'Temp_Trend': list(
+                    (None if math.isnan(v) else round(v, 2))
+                    for k, v in values_dict.items() if k != 'Actual_temp'
+                ),
+            }
+            for date, values_dict in df.to_dict('index').items()
         }
-        for date, values_dict in df.to_dict('index').items()
-    }
+    return temp_output_data
 
-    with open(OUTPUT_DIR / 'result.json', 'w') as f:
-        json.dump(output_data, f, indent=2)
-
-
-def produce_html():
-    """
-    Get data from DB and produce HTML with a table.
-    """
-    diff_of_interest = [1, 3, 12, 24, 72]
+def prepare_json_wind():
+    diff_of_interest = list(range(2, 100))
 
     max_historical_date = session.query(func.max(HistoricalData.local_date)).scalar()
     end_date = max_historical_date
-    start_date = end_date - timedelta(days=10)
+    start_date = (end_date - timedelta(days=10)).replace(hour=0)
 
     db_results = session.query(
         HistoricalData.local_date,
-        HistoricalData.temperature.label('actual_temp'),
-        ForecastData.local_date.label('forecast_date'),
-        ForecastData.temperature.label('forecast_temp'),
+        HistoricalData.wind_speed.label('actual_wind_speed'),
+        ForecastData.wind_speed.label('forecast_wind_speed'),
         ForecastData.time_difference
     ).join(
         ForecastData,
         ForecastData.local_date == HistoricalData.local_date
+    ).filter(
+        HistoricalData.local_date >= start_date,
+        ForecastData.time_difference <= max(diff_of_interest),
     ).all()
 
-    data = {}
+    wind_speed_data = {}
     for result in db_results:
-        if result.time_difference not in diff_of_interest:
-            continue
         date = result.local_date
-        if date not in data:
-            data[date] = {'Actual': result.actual_temp}
-        data[date][f'Diff -{result.time_difference}h'] = f'{result.forecast_temp} ({result.forecast_temp - result.actual_temp:.1f})'
+        if date not in wind_speed_data:
+            wind_speed_data[date] = {'Actual_wind': result.actual_wind_speed}
+            for diff_time in diff_of_interest:
+                wind_speed_data[date][diff_time] = None
+        wind_speed_data[date][result.time_difference] = (wind_speed_data[date]["Actual_wind"] / result.forecast_wind_speed) * 100.0 - 100.0
+
+    df = pd.DataFrame.from_dict(wind_speed_data, orient='index')
+    df = df.rename_axis('actual_date').reset_index()
+    df = df.groupby(df['actual_date'].dt.date).mean()
+    df = df.drop('actual_date', axis=1).sort_index(ascending=False)
     
-    for date in data.keys():
-        for diff in diff_of_interest:
-            col = f'Diff -{diff}h'
-            if col not in data[date]:
-                closest_result = None
-                closest_diff = None
-                for result in db_results:
-                    if result.local_date == date:
-                        if closest_diff is None or abs(result.time_difference - diff) < closest_diff:
-                            closest_diff = abs(result.time_difference - diff)
-                            closest_result = result
-        
-                if closest_result:
-                    data[date][col] = f'{closest_result.forecast_temp} ({closest_result.forecast_temp - data[date]["Actual"]:.1f})'
-                else:
-                    data[date][col] = '--'
 
-    df = pd.DataFrame.from_dict(data, orient='index').sort_index(ascending=False)
-    df = df[(df.index >= start_date) & (df.index <= end_date)]
-
-    
-    for diff in diff_of_interest:
-        col = f'Diff -{diff}h'
-        if col not in df.columns:
-            df[col] = '--'
-    df = df.fillna('--')
+    wind_output_data = {
+        date.strftime('%Y-%m-%d'): {
+            'Wind_Actual': f'{values_dict["Actual_wind"]:.1f}',
+            'Wind_Trend': list(
+                (None if math.isnan(v) else round(v, 2))
+                for k, v in values_dict.items() if k != 'Actual_wind'
+            ),
+        }
+        for date, values_dict in df.to_dict('index').items()
+        }
+    return wind_output_data
 
 
-    html_table = df.to_html(classes='table table-striped', border=0)
 
-    with open('./template.html', 'r') as f:
-        template_html = f.read()
-    complete_html = template_html.format(table=html_table)
+def combine_data():
+    temp_data = prepare_json_temp()
+    wind_data = prepare_json_wind()
+    for day, data_dict in wind_data.items():
+        temp_data[day].update(data_dict)
+    #     temp_data[day] = 123
+    # for day, data_dict in wind_data.items():
+    #     if day not in merged_dict:
+    #         merged_dict[day] = {}
+    #     merged_dict[day].update(data_dict)
 
-    with open(OUTPUT_DIR / 'table.html', 'w') as f:
-        f.write(complete_html)
+    with open(OUTPUT_DIR / 'result.json', 'w') as f:
+        json.dump(temp_data, f, indent=2)
 
-    # print(complete_html)
 
 if __name__ == '__main__':
     OUTPUT_DIR.mkdir(exist_ok=True, parents=True)
-    prepare_json()
+    combine_data()
     shutil.copy(Path(__file__).parent / 'anychart_template.html', OUTPUT_DIR / 'index.html')
